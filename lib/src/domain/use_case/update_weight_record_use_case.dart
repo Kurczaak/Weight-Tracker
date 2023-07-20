dbee9ee31f551bb31b167e8dbf39301874c397fa
BSS-Kurczak <m.kurczak@bsservices.co>
2023-07-20 05:36:13 +0200

    feat: add update weight record use case

---
+++ lib/src/domain/use_case/update_weight_record_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:simple_weight_tracker/src/data/repository/weight_repository.dart';
+import 'package:simple_weight_tracker/src/domain/base/base_use_case.dart';
+import 'package:simple_weight_tracker/src/domain/model/weight_record.dart';
+
+class UpdateWeightRecordUseCase extends UseCase<void, WeightRecord> {
+  UpdateWeightRecordUseCase(this._repository);
+
+  final WeightRepository _repository;
+
+  @override
+  Future<void> execute(WeightRecord param) => _repository.updateWeight(param);
+}
