6392bbb94998e46c4bdcd77f76657ba77f18e377
BSS-Kurczak <m.kurczak@bsservices.co>
2023-07-26 19:51:46 +0200

    chore: refactor WeightTrackerNotifier

--- lib/src/presentation/feature/home/notifier/weight_tracker_notifier.dart
+++ lib/src/presentation/feature/home/notifier/weight_tracker_notifier.dart
@@ -1,51 +1,84 @@
 import 'package:riverpod/riverpod.dart';
+import 'package:simple_weight_tracker/src/domain/model/data_paginator.dart';
 import 'package:simple_weight_tracker/src/domain/model/weight_record.dart';
 import 'package:simple_weight_tracker/src/domain/model/weight_record_data_paginator.dart';
-import 'package:simple_weight_tracker/src/domain/use_case/delete_weight_record_use_case.dart';
+import 'package:simple_weight_tracker/src/domain/use_case/export_use_case.dart';
 import 'package:simple_weight_tracker/src/domain/use_case/provider/use_case_provider.dart';
-import 'package:simple_weight_tracker/src/domain/use_case/save_weight_record_use_case.dart';
-import 'package:simple_weight_tracker/src/domain/use_case/watch_weight_records_use_case.dart';
+import 'package:simple_weight_tracker/src/presentation/feature/home/notifier/weight_tracker_state.dart';
 
 final weightTrackerNotifierProvider =
-    StateNotifierProvider<WeightTrackerNotifier, List<WeightRecord>>((ref) {
-  final addWeightRecord = ref.watch(saveWeightRecordUseCaseProvider);
-  final watchWeightRecords = ref.watch(watchWeightRecordsUseCaseProvider);
-  final deleteWeightRecord = ref.watch(deleteWeitghtRecordUseCaseProvider);
+    StateNotifierProvider<WeightTrackerNotifier, WeightTrackerState>((ref) {
+  final addWeightRecordUseCase = ref.watch(saveWeightRecordUseCaseProvider);
+  final watchWeightRecordsUseCase =
+      ref.watch(watchWeightRecordsUseCaseProvider);
+  final deleteWeightRecordUseCase =
+      ref.watch(deleteWeightRecordUseCaseProvider);
+  final watchInitialWeightUseCase =
+      ref.watch(watchInitialWeightUseCaseProvider);
   // Add other use case providers if they exist.
   return WeightTrackerNotifier(
-    addWeightRecord,
-    watchWeightRecords,
-    deleteWeightRecord,
+    saveWeightRecordUseCase: addWeightRecordUseCase,
+    watchWeightRecordsUseCase: watchWeightRecordsUseCase,
+    deleteWeightRecordUseCase: deleteWeightRecordUseCase,
+    watchInitialWeightUseCase: watchInitialWeightUseCase,
   );
 });
 
-class WeightTrackerNotifier extends StateNotifier<List<WeightRecord>> {
-  WeightTrackerNotifier(
-    this._saveWeightRecordUseCase,
-    this._watchWeightRecordsUseCase,
-    this._deleteWeightRecordUseCase,
-  ) : super([]) {
+class WeightTrackerNotifier extends StateNotifier<WeightTrackerState> {
+  WeightTrackerNotifier({
+    required this.saveWeightRecordUseCase,
+    required this.watchWeightRecordsUseCase,
+    required this.deleteWeightRecordUseCase,
+    required this.watchInitialWeightUseCase,
+  }) : super(const WeightTrackerState()) {
     _watchWeights();
   }
 
-  final SaveWeightRecordUseCase _saveWeightRecordUseCase;
-  final WatchWeightRecordsUseCase _watchWeightRecordsUseCase;
-  final DeleteWeightRecordUseCase _deleteWeightRecordUseCase;
+  static const _pageSize = 10;
+
+  final SaveWeightRecordUseCase saveWeightRecordUseCase;
+  final WatchWeightRecordsUseCase watchWeightRecordsUseCase;
+  final DeleteWeightRecordUseCase deleteWeightRecordUseCase;
+  final WatchInitialWeightUseCase watchInitialWeightUseCase;
 
   Future<void> addRecord(WeightRecord record) =>
-      _saveWeightRecordUseCase(record.copyWith(id: null));
+      saveWeightRecordUseCase(record.copyWith(id: null));
+
+  Future<void> updateRecord(WeightRecord record) =>
+      saveWeightRecordUseCase(record.copyWith());
 
   void removeRecord(WeightRecord record) =>
-      _deleteWeightRecordUseCase.call(record);
+      deleteWeightRecordUseCase.call(record);
 
   Future<void> _watchWeights() async {
-    final records = await _watchWeightRecordsUseCase(
-      const WeightRecordDataPaginator(),
+    final latestRecords = await watchWeightRecordsUseCase(
+      const WeightRecordDataPaginator(
+        dataPaginator: DataPaginator(pageNumber: 0, pageSize: _pageSize),
+      ),
     );
-    records.fold(
-      (l) => state = [],
+
+    final initiaRecord = await watchInitialWeightUseCase();
+
+    initiaRecord.fold(
+      (l) => state = state.copyWith(
+        isError: true,
+      ),
       (r) => r.listen((event) {
-        state = event;
+        state = state.copyWith(
+          initialWeight: event,
+        );
+      }),
+    );
+
+    latestRecords.fold(
+      (l) => state = state.copyWith(
+        isError: true,
+      ),
+      (r) => r.listen((event) {
+        state = state.copyWith(
+          records: event,
+          latestWeight: event.first,
+        );
       }),
     );
   }
