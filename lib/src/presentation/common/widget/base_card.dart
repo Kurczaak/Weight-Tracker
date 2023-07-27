a17ef5bad899d38fc5cab70d960137a42358802a
BSS-Kurczak <m.kurczak@bsservices.co>
2023-07-27 19:44:52 +0200

    feat: add base card widget

---
+++ lib/src/presentation/common/widget/base_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:simple_weight_tracker/src/presentation/styleguide/app_dimens.dart';
+
+class BaseCard extends StatelessWidget {
+  const BaseCard({
+    required this.child,
+    super.key,
+    this.padding,
+    this.height,
+    this.width,
+  });
+  final Widget child;
+  final EdgeInsets? padding;
+  final double? height;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppDimens.baseRadius),
+        ),
+        child: Padding(
+          padding: padding ?? EdgeInsets.zero,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
