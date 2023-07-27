4cf0c46cefe4f769d2aac5b38c2062802c5c8eea
BSS-Kurczak <m.kurczak@bsservices.co>
2023-07-27 08:16:10 +0200

    feat: add 'beginingOfCurrentMonth' and 'getMonthsAgo' date time extensions

--- lib/src/utils/date_time_extensions.dart
+++ lib/src/utils/date_time_extensions.dart
@@ -8,4 +8,10 @@ extension DateTimeExtensions on DateTime {
   }
 
   DateTime get startOfDay => DateTime(year, month, day);
+
+  DateTime get beginingOfCurrentMonth => DateTime(year, month);
+
+  DateTime getMonthsAgo(int months) {
+    return DateTime(year, month - months);
+  }
 }
