0b6b48368f348714ff5c072e3f8b2270fd4ceec0
BSS-Kurczak <m.kurczak@bsservices.co>
2023-07-27 19:44:44 +0200

    feat: add app dimens

---
+++ lib/src/presentation/styleguide/app_dimens.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class AppDimens {
+  AppDimens._();
+  static const double padding2 = 2;
+  static const double padding4 = 4;
+  static const double padding8 = 8;
+  static const double padding12 = 12;
+  static const double padding16 = 16;
+  static const double padding20 = 20;
+  static const double padding24 = 24;
+  static const double padding32 = 32;
+  static const double padding40 = 40;
+  static const double padding48 = 48;
+  static const double padding52 = 52;
+  static const double padding56 = 56;
+  static const double padding64 = 64;
+  static const double padding72 = 72;
+  static const double padding80 = 80;
+
+  static const double margin2 = 2;
+  static const double margin4 = 4;
+  static const double margin8 = 8;
+  static const double margin12 = 12;
+  static const double margin16 = 16;
+  static const double margin20 = 20;
+  static const double margin24 = 24;
+  static const double margin32 = 32;
+  static const double margin40 = 40;
+  static const double margin48 = 48;
+  static const double margin52 = 52;
+  static const double margin56 = 56;
+  static const double margin64 = 64;
+  static const double margin72 = 72;
+  static const double margin80 = 80;
+
+  // paddings
+  static const paddingSmall = padding4;
+  static const paddingMedium = padding12;
+  static const paddingLarge = padding24;
+  static const paddingXLarge = padding48;
+
+  // radiuses
+  static const double baseRadius = 25;
+
+  static const double periodPickerWidth = 200;
+}
+
+class AppSpacers {
+  AppSpacers._();
+
+  // height
+  static const SizedBox h2 = SizedBox(height: AppDimens.margin2);
+  static const SizedBox h4 = SizedBox(height: AppDimens.margin4);
+  static const SizedBox h8 = SizedBox(height: AppDimens.margin8);
+  static const SizedBox h12 = SizedBox(height: AppDimens.margin12);
+  static const SizedBox h16 = SizedBox(height: AppDimens.margin16);
+  static const SizedBox h20 = SizedBox(height: AppDimens.margin20);
+  static const SizedBox h24 = SizedBox(height: AppDimens.margin24);
+  static const SizedBox h32 = SizedBox(height: AppDimens.margin32);
+  static const SizedBox h40 = SizedBox(height: AppDimens.margin40);
+  static const SizedBox h48 = SizedBox(height: AppDimens.margin48);
+  static const SizedBox h52 = SizedBox(height: AppDimens.margin52);
+  static const SizedBox h56 = SizedBox(height: AppDimens.margin56);
+  static const SizedBox h64 = SizedBox(height: AppDimens.margin64);
+  static const SizedBox h72 = SizedBox(height: AppDimens.margin72);
+  static const SizedBox h80 = SizedBox(height: AppDimens.margin80);
+
+  // width
+  static const SizedBox w2 = SizedBox(width: AppDimens.margin2);
+  static const SizedBox w4 = SizedBox(width: AppDimens.margin4);
+  static const SizedBox w8 = SizedBox(width: AppDimens.margin8);
+  static const SizedBox w12 = SizedBox(width: AppDimens.margin12);
+  static const SizedBox w16 = SizedBox(width: AppDimens.margin16);
+  static const SizedBox w20 = SizedBox(width: AppDimens.margin20);
+  static const SizedBox w24 = SizedBox(width: AppDimens.margin24);
+  static const SizedBox w32 = SizedBox(width: AppDimens.margin32);
+  static const SizedBox w40 = SizedBox(width: AppDimens.margin40);
+  static const SizedBox w48 = SizedBox(width: AppDimens.margin48);
+  static const SizedBox w52 = SizedBox(width: AppDimens.margin52);
+  static const SizedBox w56 = SizedBox(width: AppDimens.margin56);
+  static const SizedBox w64 = SizedBox(width: AppDimens.margin64);
+  static const SizedBox w72 = SizedBox(width: AppDimens.margin72);
+  static const SizedBox w80 = SizedBox(width: AppDimens.margin80);
+}
