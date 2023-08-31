daf4c07249248eb91cd8bbd89f00a6836eee9691
BSS-Kurczak <m.kurczak@bsservices.co>
2023-08-31 19:48:47 +0200

    feat: add goal weight related use cases provider

--- lib/src/domain/use_case/provider/use_case_provider.dart
+++ lib/src/domain/use_case/provider/use_case_provider.dart
@@ -1,6 +1,9 @@
 import 'package:riverpod/riverpod.dart';
+import 'package:simple_weight_tracker/src/data/providers/config_repository_provider.dart';
 import 'package:simple_weight_tracker/src/data/providers/weight_repository_provider.dart';
 import 'package:simple_weight_tracker/src/domain/use_case/export_use_case.dart';
+import 'package:simple_weight_tracker/src/domain/use_case/get_goal_weight.dart';
+import 'package:simple_weight_tracker/src/domain/use_case/save_goal_weight_use_case.dart';
 
 // CRUD operations
 
@@ -9,6 +12,11 @@ final saveWeightRecordUseCaseProvider =
     Provider<SaveWeightRecordUseCase>((ref) {
   final repo = ref.watch(weightRepositoryProvider);
   return SaveWeightRecordUseCase(repo);
+});
+
+final saveGoalWeightUseCaseProvider = Provider<SaveGoalWeightUseCase>((ref) {
+  final repo = ref.watch(configRepositoryProvider);
+  return SaveGoalWeightUseCase(repo);
 });
 
 // READ
@@ -21,6 +29,11 @@ final watchInitialWeightUseCaseProvider =
     Provider<WatchInitialWeightUseCase>((ref) {
   final repo = ref.watch(weightRepositoryProvider);
   return WatchInitialWeightUseCase(repo);
+});
+
+final getGoalWeightUseCaseProvider = Provider<GetGoalWeightUseCase>((ref) {
+  final repo = ref.watch(configRepositoryProvider);
+  return GetGoalWeightUseCase(repo);
 });
 
 // UPDATE
