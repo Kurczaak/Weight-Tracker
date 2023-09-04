285ad7faa4e8a6e5c016218fc9df272ba3310319
BSS-Kurczak <m.kurczak@bsservices.co>
2023-09-04 18:40:52 +0200

    chore: extract animation duration const

--- lib/src/presentation/styleguide/app_consts.dart
+++ lib/src/presentation/styleguide/app_consts.dart
@@ -19,4 +19,8 @@ class AppConsts {
 
   // String related
   static const String emptyPlaceholder = '-';
+
+  // Durations
+  static const Duration bottomBarNavigationDuration =
+      Duration(milliseconds: 300);
 }
