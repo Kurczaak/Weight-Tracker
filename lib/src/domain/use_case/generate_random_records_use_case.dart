1cf5bbda0a1e39a906858c965fee350552fcd66a
BSS-Kurczak <m.kurczak@bsservices.co>
2023-09-05 07:27:43 +0200

    chore: extract double extensions

--- lib/src/domain/use_case/generate_random_records_use_case.dart
+++ lib/src/domain/use_case/generate_random_records_use_case.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:simple_weight_tracker/src/data/repository/weight_repository.dart';
 import 'package:simple_weight_tracker/src/domain/base/base_use_case.dart';
 import 'package:simple_weight_tracker/src/domain/model/weight_record.dart';
+import 'package:simple_weight_tracker/src/utils/double_extensions.dart';
 
 /// Debug purposes only
 /// Generates random records given a number of days
@@ -15,12 +14,12 @@ class GenerateRandomRecordsUseCase extends UseCase<void, int> {
   Future<void> execute(int param) async {
     var a = 0.0;
     for (var i = 0; i < param; i++) {
-      final random = Random();
-      a += random.nextDouble() - .5;
+      final randomProgress = DoubleExtensions.getRandomInRange(-.5, .5);
+      a += randomProgress;
       if (i.isEven) {
         await _repository.addWeight(
           WeightRecord(
-            weight: 80 + a,
+            weight: 80 + a.roundTo(1),
             date: DateTime.now().subtract(Duration(days: i)),
           ),
         );
