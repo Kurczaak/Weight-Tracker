de16985058c3b44baade43539eb68ebf18bb04cb
BSS-Kurczak <m.kurczak@bsservices.co>
2023-09-13 19:59:33 +0200

    chore: remove old mean weight model

--- lib/src/domain/model/export_model.dart
+++ lib/src/domain/model/export_model.dart
@@ -2,5 +2,5 @@ export 'data_paginator.dart';
 export 'date_boundaries.dart';
 export 'group_records_param.dart';
 export 'user_config.dart';
-export 'weight_record.dart';
+export 'weight/weight_record.dart';
 export 'weight_record_data_paginator.dart';
