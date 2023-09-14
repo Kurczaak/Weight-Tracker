407919ab29179ce64bbc2a5b89e180a08e863b46
BSS-Kurczak <m.kurczak@bsservices.co>
2023-09-14 16:30:41 +0200

    feat: add 'meanWeight' extension for List of WeightRecords

--- lib/src/utils/weight_record_extensions.dart
+++ lib/src/utils/weight_record_extensions.dart
@@ -1,6 +1,14 @@
-import 'package:simple_weight_tracker/src/domain/model/weight_record.dart';
+import 'package:simple_weight_tracker/src/domain/model/export_model.dart';
 
 extension WeightRecordExtension on List<WeightRecord> {
   List<WeightRecord> get orderedByDate =>
       [...this]..sort((a, b) => a.date.compareTo(b.date));
+
+  double? get meanWeight => isEmpty
+      ? null
+      : fold<double>(
+            0,
+            (previousValue, element) => previousValue + element.weight,
+          ) /
+          length;
 }
