a4611a28a4570ca8b950826c5171312444850a97
BSS-Kurczak <m.kurczak@bsservices.co>
2023-09-14 17:51:45 +0200

    feat: add weight record ui model mappers

--- lib/src/presentation/common/model/weight_record_ui_model.dart
+++ lib/src/presentation/common/model/weight_record_ui_model.dart
@@ -1,4 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:simple_weight_tracker/src/domain/model/weight/mean_weight.dart';
+import 'package:simple_weight_tracker/src/domain/model/weight/weight_record.dart';
 
 part 'weight_record_ui_model.freezed.dart';
 
@@ -7,7 +9,8 @@ class WeightRecordUIModel with _$WeightRecordUIModel {
   const factory WeightRecordUIModel({
     required double weight,
     required DateTime toDate,
-    required int id,
+    required int? id,
+    WeightRecordUIModelType? type,
     DateTime? fromDate,
   }) = _WeightRecordUIModel;
 }
@@ -16,4 +19,23 @@ enum WeightRecordUIModelType {
   daily,
   weekly,
   monthly,
+}
+
+extension WeighRecordUIModelMappers on WeightRecordUIModel {
+  WeightRecordUIModel fromWeightRecord(WeightRecord weightRecord) =>
+      WeightRecordUIModel(
+        weight: weightRecord.weight,
+        toDate: weightRecord.date,
+        id: weightRecord.id,
+        type: WeightRecordUIModelType.daily,
+      );
+
+  WeightRecordUIModel fromMeanWeight(MeanWeight meanWeight) =>
+      WeightRecordUIModel(
+        weight: meanWeight.meanWeight ?? 0,
+        toDate: meanWeight.period.toDate,
+        fromDate: meanWeight.period.fromDate,
+        id: meanWeight.id,
+        type: WeightRecordUIModelType.daily,
+      );
 }
